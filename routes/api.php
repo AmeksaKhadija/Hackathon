080b055bff9d0beacac58cad85b6513fbdbafd18
Ameksa khadija <ameksakhadija026@gmail.com>
2025-03-19 10:38:09 +0000

    diagramme de class + authentification

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\JWTAuthController;
+use App\Http\Middleware\JwtMiddleware;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::post('register', [JWTAuthController::class, 'register']);
+Route::post('login', [JWTAuthController::class, 'login']);
+
+Route::middleware([JwtMiddleware::class])->group(function () {
+    Route::get('user', [JWTAuthController::class, 'getUser']);
+    Route::post('logout', [JWTAuthController::class, 'logout']);
+});
